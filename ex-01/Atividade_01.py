34982b32297f027ba03de242748238341a8930b0
Gabriel Buchud <gabrielbuchudestudos@gmail.com>
2025-10-20 13:57:00 -0300

    Atividade 01 resolvida

---
+++ ex-01/Atividade_01.py
@@ -0,0 +1,23 @@
+# 1 - Programa de Saudação
+print("Olá, mundo!")
+
+# 2 - Calculadora de Soma
+numero1, numero2 = 12, 14
+soma = numero1 + numero2
+print(f"A soma entre {numero1} e {numero2} é {soma}")
+
+# 3 - Calculadora de Volume
+comprimento, largura, altura = 12, 14, 20
+volume = comprimento * largura * altura
+print(f"O volume da caixa é {volume} cm³")
+
+# 4 - Calculadora de Preço Total
+nome_produto = "carrinho de brinquedo"
+preco_unitario = 19.90
+quantidade = 3
+preco_total = preco_unitario * quantidade
+
+print(f"Produto: {nome_produto}")
+print(f"Preço unitário: R$ {preco_unitario:.2f}")
+print(f"Quantidade: {quantidade}")
+print(f"Preço total: R$ {preco_total:.2f}")
